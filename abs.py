81387816117287224a6fef0a45380677d034dee2
superhan <chenchaohan@outlook.com>
2017-12-12 21:27:51 +0800

    add 2files

--- abs.py
+++ abs.py
@@ -1,5 +1,6 @@
 # print absolute value of an integer:
-L = [ ['Apple', 'Google', 'Microsoft'], ['Java', 'Python', 'Ruby', 'PHP'], ['Adam', 'Bart', 'Lisa'] ] 
+from array import array
+L = [ ['Apple', 'Google', 'Microsoft'], ['Java', 'Python', 'Ruby', 'PHP'], ['Adam', 'Bart', 'Lisa'] ]
 print(L[0][0]) 
 print(L[1][1])
 print(L[2][2])
